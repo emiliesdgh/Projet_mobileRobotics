c3dc80b95e4c2721511eddd697d197018ae172c3
em <102719562+emiliesdgh@users.noreply.github.com>
2023-11-21 18:20:39 +0100

    commun files to modify all

---
+++ main.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import time
+
+#import the classes from the other modules
+from local_navigation import LocalNavigation
+from filtering import Filtering
+
+
+while(1) :
+
+    a = 0
+
+
+
+
+#class Main :
+
+   # def __init__() :
