e6179be279e52d57f62bbf527b2be7358aac2ee9
em <emilie.grandjean@epfl.ch>
2023-11-21 18:32:55 +0100

    new fils w/  just odometry

---
+++ classes.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import time
+
+#import the classes from the other modules
+from local_navigation import LocalNavigation
+from filtering import Filtering
+
+class Classes :
+
+    def __init__(self) :
+
+        self.variable = 0 
