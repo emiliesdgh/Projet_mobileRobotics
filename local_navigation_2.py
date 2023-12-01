3c0999d46e59bca8127c239e7fe11d6097da599a
em <emilie.grandjean@epfl.ch>
2023-12-01 09:03:35 +0100

    sans les AW

--- local_navigation_2.py
+++ local_navigation_2.py
@@ -1,7 +1,8 @@
 import classes
-from tdmclient import ClientAsync, aw
-client = ClientAsync()
-node = aw(client.wait_for_node())
+from classes import Thymio
+#from tdmclient import ClientAsync, aw
+#client = ClientAsync()
+#node = aw(client.wait_for_node())
 
 import numpy as np
 
@@ -24,15 +25,21 @@ def motors(l_speed=500, r_speed=500, verbose=False):
         "motor.right.target": [r_speed],
     }
 
-if test_functions:
-    aw(node.set_variables(motors(100, 100))) #test with lower speed value
-    aw(client.sleep(2))
-    aw(node.set_variables(motors(0, 0)))
+def test_the_fct (Thymio, node, client, test_functions) :
+    if test_functions:
+        #aw(node.set_variables(motors(100, 100))) #test with lower speed value
+        Thymio.setSpeedRight(100,node)
+        Thymio.setSpeedLeft(100,node)
+        #aw(client.sleep(2))
+        client.sleep(2)
+        #aw(node.set_variables(motors(0, 0)))
+        Thymio.setSpeedRight(0,node)
+        Thymio.setSpeedLeft(0,node)
 
 
 #test 28.11.23 - 16h30
 
-def test_saw_osb(obs_threshold, verbose=False): ## fonction necessaire !
+def test_saw_osb(obs_threshold, node, verbose=False): ## fonction necessaire !
     """
     Tests whether one of the proximity sensors saw a wall
     param wall_threshold: threshold starting which it is considered that the sensor saw a wall
@@ -44,7 +51,7 @@ def test_saw_osb(obs_threshold, verbose=False): ## fonction necessaire !
     
     return False
 
-def clockwise(verbose=False) :
+def clockwise(node, verbose=False) :
 
     prox = list(node["prox.horizontal"]) + [0]
     print(prox[1])
@@ -54,7 +61,7 @@ def clockwise(verbose=False) :
     
     return False
 
-async def obstacle_avoidance(motor_speed=100, obs_threshold=500, verbose=False): #, clockwise = False):
+def obstacle_avoidance(client, node, motor_speed=100, obs_threshold=500, verbose=False): #, clockwise = False):
     """
     Wall following behaviour of the FSM
     param motor_speed: the Thymio's motor speed
@@ -70,7 +77,8 @@ async def obstacle_avoidance(motor_speed=100, obs_threshold=500, verbose=False):
     clockwise_true = False
     
     if verbose: print("\t Moving forward")
-    aw(node.set_variables(motors(motor_speed, motor_speed)))
+    #aw(node.set_variables(motors(motor_speed, motor_speed)))
+    node.set_variables(motors(motor_speed, motor_speed))
            
     prev_state="forward"
     
@@ -88,13 +96,15 @@ async def obstacle_avoidance(motor_speed=100, obs_threshold=500, verbose=False):
 
                     if clockwise(verbose=False) :
                         #await print_sensor_values('prox.horizontal')
-                        aw(node.set_variables(motors(motor_speed, -motor_speed)))
+                        #aw(node.set_variables(motors(motor_speed, -motor_speed)))
+                        node.set_variables(motors(motor_speed, -motor_speed))
                         print("\tSaw wall, turning clockwise CLOCCCKKK")
                         clockwise_true = True
 
                     else :
 
-                        aw(node.set_variables(motors(-motor_speed, motor_speed)))
+                        #aw(node.set_variables(motors(-motor_speed, motor_speed)))
+                        node.set_variables(motors(-motor_speed, motor_speed))
                         print("\tSaw wall, turning COUNTEERRR counterclockwise")
                     
                 prev_state="turning"
@@ -105,27 +115,45 @@ async def obstacle_avoidance(motor_speed=100, obs_threshold=500, verbose=False):
                     print("\t Contourning obstacle")
 
                     if clockwise_true :
-                        aw(node.set_variables(motors(motor_speed-40, motor_speed)))
+                        #aw(node.set_variables(motors(motor_speed-40, motor_speed)))
+                        node.set_variables(motors(motor_speed-40, motor_speed))
                         prev_state="forward"
                         print("\t CLOOOOCCCKKK Clockwise")
-                        aw(client.sleep(18))
-                        aw(node.set_variables(motors(motor_speed, -motor_speed)))
-                        aw(client.sleep(2))
+                        #aw(client.sleep(18))
+                        client.sleep(18)
+                        #aw(node.set_variables(motors(motor_speed, -motor_speed)))
+                        node.set_variables(motors(motor_speed, -motor_speed))
+                        #aw(client.sleep(2))
+                        client.sleep(2)
                         obs_avoided = True
                         
                     else :
-                        aw(node.set_variables(motors(motor_speed, motor_speed-40)))
+                        #aw(node.set_variables(motors(motor_speed, motor_speed-40)))
+                        node.set_variables(motors(motor_speed, motor_speed-40))
                         prev_state="forward"
                         print("\t COUNTEEERRR Counterclockwise")
-                        aw(client.sleep(18))
-                        aw(node.set_variables(motors(-motor_speed, motor_speed)))
-                        aw(client.sleep(2))
+                        #aw(client.sleep(18))
+                        client.sleep(18)
+                        #aw(node.set_variables(motors(-motor_speed, motor_speed)))
+                        node.set_variables(motors(-motor_speed, motor_speed))
+                        #aw(client.sleep(2))
+                        client.sleep(2)
                         obs_avoided = True
 
-        aw(client.sleep(0.1)) #otherwise, variables would not be updated
+        #aw(client.sleep(0.1)) #otherwise, variables would not be updated
+        client.sleep(0.1)
     return 
 
-if test_functions:
-    aw(obstacle_avoidance(verbose=True))
-    aw(node.set_variables(motors(0, 0)))
+'''if test_functions:
+    #aw(obstacle_avoidance(verbose=True))
+    obstacle_avoidance(verbose=True)
+    #aw(node.set_variables(motors(0, 0)))
+    node.set_variables(motors(0, 0))'''
+
+def test_the_fct (Thymio, node, client, test_functions) :
+    if test_functions:
+        obstacle_avoidance(verbose=True)
+        #aw(node.set_variables(motors(0, 0)))
+        Thymio.setSpeedRight(0,node)
+        Thymio.setSpeedLeft(0,node)
 
