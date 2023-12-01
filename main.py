debee3b89eb694befc950c3f0e7ffeb596c7b4b0
juanmartincampos <juan.martincampos@epfl.ch>
2023-12-01 10:01:52 +0100

    Changes tomain and global_navigation for testing

--- main.py
+++ main.py
@@ -11,6 +11,25 @@ from filtering import KalmanFilter
 from vision import *
 from global_visibility import *
 
+test_occupancy_grid = np.array([[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0],
+                                [0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0],
+                                [0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0],
+                                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+                                [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+                                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]])
+goal = (0,0)
+start = (10,15)
+
+
 while(1) :
 
     a = 0
@@ -25,20 +44,22 @@ while(1) :
 
     robot = Thymio()
 
-    vision = Vision()
-    vision.capture_image()
-    vision.find_goal_pos()
-    vision.find_start_pos()
-    vision.find_angle(robot)
-    vision.find_corners()
-    vision.trace_contours()
-    vision.compute_dist_mx(robot)
+#     vision = Vision()
+#     vision.capture_image()
+#     vision.find_goal_pos()
+#     vision.find_start_pos()
+#     vision.find_angle(robot)
+#     vision.find_corners()
+#     vision.trace_contours()
+#     vision.compute_dist_mx(robot)
     
-    global_nav = Global_Nav()
-    global_nav.dijkstra(robot)
-    global_nav.extract_path(robot)
+#     global_nav = Global_Nav()
+#     global_nav.dijkstra(robot)
+#     global_nav.extract_path(robot)
 
-
+      global_nav = globalNavigation()
+      path, visitedNodes = global_nav.A_star(start, goal, test_occupancy_grid)
+      
 
 
 
